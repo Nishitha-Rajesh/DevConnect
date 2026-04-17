459e9b9a21003e539c15eb202b9c64a21c8bd45c
Nishitha-Rajesh <nishitharajesh25@gmail.com>
2026-04-17 06:05:43 +0530

    exploring get,post ,patch...

--- src/app.js
+++ src/app.js
@@ -3,11 +3,24 @@ const app = express();
 /*app.use((req, res) => {
     res.send("hello from server");
 });*/
+app.get("/user", (req, res) => {
+    res.send({ firstname: "Nishitha" });
+});
+app.post("/user", (req, res) => {
+    res.send("saved data to database succesfully");
+})
 app.use("/hello", (req, res) => {
     res.send("hello hello  hello from server");
 });
+// use method will  all the http methods api call 
 app.use("/test", (req, res) => {
     res.send("test test ");
+});
+app.use("/test/exam", (req, res) => {
+    res.send("test exam ");
+});
+app.use("/", (req, res) => {
+    res.send("any path ");
 });
 app.listen(7777, () => {
     console.log("server is succesfully listening on port 7777");
